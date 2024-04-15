3e29ab9a756086f6b7ea1295698187d9d25887a7
kleber-matos <94052079+kleber-matos@users.noreply.github.com>
2024-04-15 12:15:35 -0300

    :ok_hand: Alterações de revisão de código

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,13 @@ import Header from "../../components/Header/Header";
 import axios from "axios";
 import CardFilme from "../../components/Card/CardFilme";
 
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+
 export default function Home() {
   const [filme, setFilme] = useState([]);
 
@@ -27,6 +34,27 @@ export default function Home() {
   return (
     <>
       <Header />
+
+      <Swiper
+        modules={[Pagination, Autoplay]}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 1000 }}
+      >
+        {filme.map((item) => (
+          <SwiperSlide>
+            <img
+              style={{
+                width: "100%",
+                height: "60vh",
+                backgroundColor: "black",
+                opacity: ".3",
+              }}
+              src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`}
+              alt=""
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
       <S.SpaceEvenly>
         {filme.map((item, id) => (
