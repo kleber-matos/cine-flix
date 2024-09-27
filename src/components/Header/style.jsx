8498d2ada765f88f99ddc9e06cba28ec6957f370
kleber-matos <klebermatos2019@gmail.com>
2024-09-27 13:30:36 -0300

    🧹cleanup: Limpeza de Código

--- src/components/Header/style.jsx
+++ src/components/Header/style.jsx
@@ -1,94 +1,65 @@
-/** @format */
-
-import styled, { css } from "styled-components";
-
-const FlexBox = css`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
+import styled from "styled-components";
 
 export const Header = styled.header`
-  position: relative;
-  width: 100%;
-
-  margin: 2rem 0 2rem 0;
+  display: flex;
+  justify-content: space-between;
   align-items: center;
-  z-index: 1;
-  /* background-color: #000000; */
+  margin: 3rem 0 3rem 0;
 
-  .logo {
-    font-size: clamp(1.2rem, 10vw, 2rem);
-    /* margin-left: 1rem; */
-    &:hover {
-      border: none;
-    }
-  }
-
-  h2 {
-    margin-left: 1rem;
-  }
-
-  section {
-    ${FlexBox}
-    justify-content: space-between;
-  }
-  button {
-    background-color: transparent;
-    color: #ffffff;
-    border: none;
-    font-size: clamp(1.7rem, 10vw, 3rem);
-    cursor: pointer;
-    margin-right: 0.5rem;
-  }
-
-  @media (min-width: 700px) {
-    button {
-      display: none;
-    }
-  }
-
-  a:hover {
-    border-bottom: solid;
-  }
-`;
-
-export const NavDesktop = styled.div`
-  ul {
-    ${FlexBox}
-    justify-content: space-between;
-    height: 10vh;
-    width: 25vw;
-  }
-
-  @media (max-width: 699px) {
-    display: none;
-  }
-`;
-
-export const NavMobile = styled.div`
-  text-align: center;
-
-  nav {
-    ${FlexBox}
-    width: 100%;
-    height: 60vh;
-    position: absolute;
-    backdrop-filter: blur(10px);
-    /* background-color: #000000; */
-    position: relative;
-  }
-
-  ul {
-    height: 70%;
-    display: flex;
+  @media (max-width: 700px) {
+    /* border: solid red; */
     flex-direction: column;
-    justify-content: space-between;
+    justify-content: center;
+    margin: 2rem 0 2rem 0;
   }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  /* border: solid yellow; */
+  width: 50%;
+  @media (max-width: 700px) {
+    width: 100%;
+  }
+`;
+
+export const Logo = styled.h1`
+  font-size: 1.7rem;
+`;
+
+export const Menu = styled.ul`
+  display: flex;
+  width: 40vw;
+  max-width: 500px;
+  justify-content: space-between;
 
   a {
-    font-size: clamp(1.5rem, 10vw, 2rem);
+    font-size: 1.3rem;
   }
+
+  @media (max-width: 700px) {
+    height: 30vh;
+    justify-content: space-evenly;
+    align-items: end;
+    flex-direction: column;
+    display: ${({ isOpen }) => (isOpen ? "flex" : "none")};
+    width: 100%;
+
+    li {
+      border-bottom: solid 4px;
+      width: 100%;
+    }
+  }
+`;
+
+export const ToggleButton = styled.button`
+  background: transparent;
+  border: none;
+  color: white;
+  cursor: pointer;
+  font-size: 2.3rem;
 
   @media (min-width: 700px) {
     display: none;
