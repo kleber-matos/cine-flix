25d0492124a62b9c20913403acf862208c58e612
kleber-matos <94052079+kleber-matos@users.noreply.github.com>
2024-10-06 18:37:16 -0300

    💄 feat: Estilização CSS da Rota de Assistir

--- src/components/Card/style.jsx
+++ src/components/Card/style.jsx
@@ -5,6 +5,7 @@ const BreackText = css`
   display: -webkit-box;
   -webkit-box-orient: vertical;
   overflow: hidden;
+  -webkit-line-clamp: 4;
 `;
 
 const Flexbox = css`
@@ -13,172 +14,55 @@ const Flexbox = css`
   align-items: center;
 `;
 
-export const Card = styled.div`
-  width: 25vw;
-  max-width: 300px;
-  height: 55vh;
-  margin: 1rem;
-  margin-top: 2rem;
-  text-align: center;
-  border: solid 0.5px #3333336c;
-  overflow: hidden;
-
-  .title {
-    ${BreackText}
-    -webkit-line-clamp: 1;
-    color: white;
-    height: 20vh;
-    display: flex;
-
-    p {
-      font-size: 1.2rem;
-    }
-  }
-
-  div {
-    ${Flexbox}
-    justify-content: space-evenly;
-    flex-direction: column;
-    height: 25%;
-    width: 100%;
-  }
-
-  h2 {
-    ${BreackText}
-    -webkit-line-clamp: 2;
-  }
-
-  button {
-    width: 80%;
-    height: 4vh;
-    border: none;
-    font-weight: 700;
-    border-radius: 4px;
-    background-color: red;
-    color: white;
-    cursor: pointer;
-  }
-
-  img {
-    width: 100%;
-    height: 70%;
-  }
-
-  @media (max-width: 700px) {
-    width: 90vw;
-    button {
-      height: 40%;
-    }
-
-    div {
-      height: 30%;
-      width: 100%;
-      padding-bottom: 1rem;
-    }
-
-    .title {
-      p {
-        font-size: 1rem;
-      }
-    }
-  }
-`;
-
-export const Assistir = styled.section`
-  ${Flexbox}
-  flex-direction: column;
-  align-items: start;
-
-  /* background-color: black; */
-  height: 95vh;
-
+export const Container = styled.section`
   img {
     width: 100%;
     height: 60vh;
-  }
-
-  h2 {
-    text-align: center;
-    color: red;
-  }
-
-  @media (max-width: 700px) {
-    flex-direction: column;
-    justify-content: space-evenly;
-    align-items: center;
-    img {
-      width: 100%;
-      height: 40%;
-    }
+    opacity: 0.7;
   }
 `;
-export const About = styled.section`
-  width: 60%;
-  height: 40vh;
+
+export const Info = styled.div`
+  text-align: center;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  align-items: center;
 
-  .sinopse {
-    display: -webkit-box;
-    -webkit-box-orient: vertical;
-    overflow: hidden;
-    -webkit-line-clamp: 7;
-
-    color: #a8a8a8;
+  p,
+  span {
+    ${BreackText}
+    margin: 10px 0 20px 0;
   }
 
-  @media (max-width: 700px) {
+  a {
     width: 100%;
-    justify-content: space-evenly;
-    align-items: center;
-    text-align: center;
-    p {
-      font-size: clamp(1rem, 10vw, 0.5rem);
+    height: 5vh;
+    font-size: 1.4rem;
+    font-weight: 600;
+    background-color: red;
+    color: white;
+    border-radius: 10px;
+    max-width: 300px;
+    margin-bottom: 10px;
+    cursor: pointer;
+    ${Flexbox}
+    &:hover {
+      background-color: #ff0000b7;
     }
-  }
-
-  @media (min-width: 701px) {
-    width: 40%;
   }
 `;
 
-export const Iframe = styled.div`
-  position: relative;
+export const PlayMovie = styled.section`
   iframe {
-    border: solid black;
-    height: 100vh;
     width: 100%;
-  }
+    height: 100vh;
 
-  @media (max-width: 700px) {
-    iframe {
-      height: 40vh;
+    @media (max-width: 910px) {
+      height: 50vh;
     }
   }
-`;
 
-export const Button = styled.a`
-  ${Flexbox}
-  background-color: red;
-  width: 50%;
-  height: 13%;
-  border-radius: 5px;
-  text-decoration: none;
-  color: white;
-  font-weight: 600;
-  &:hover {
-    background-color: #ff0000c8;
+  p {
+    margin: 20px 0 20px 0;
   }
-
-  @media (max-width: 700px) {
-    width: 80%;
-    height: 7vh;
-    margin-bottom: 1rem;
-  }
-`;
-
-export const SubTtitle = styled.h2`
-  font-size: clamp(1.2rem, 10vw, 2rem);
-  text-align: center;
 `;
